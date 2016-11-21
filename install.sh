9b1c6b82a8a31c4355727f45af2890824aaf012f
redaxmedia <info@redaxmedia.com>
2016-11-21 14:24:08 +0100

    Install process part3

--- install.sh
+++ install.sh
@@ -2,6 +2,7 @@
 
 cd build
 php console.php config parse --db-url $DB_URL
+php console.php uninstall database
 php console.php install database --admin-name admin --admin-user admin --admin-password admin --admin-email admin@localhost
 php console.php install module --alias Demo
 php console.php status database
