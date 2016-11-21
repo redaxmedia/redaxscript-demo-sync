a77ae992791a373a682d552a2519720570fc3f3b
redaxmedia <info@redaxmedia.com>
2016-11-21 14:16:51 +0100

    Install process part2

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 cd build
-php console.php config parse --db-url sqlite://demo.sqlite
+php console.php config parse --db-url $DB_URL
 php console.php install database --admin-name admin --admin-user admin --admin-password admin --admin-email admin@localhost
 php console.php install module --alias Demo
 php console.php status database
