a64e0572c3b2184ae9874e4f5fb5894df3628b54
redaxmedia <info@redaxmedia.com>
2018-05-11 19:38:38 +0200

    Try the brand new SFTP part2

--- tasks/ftpress.js
+++ tasks/ftpress.js
@@ -14,7 +14,7 @@ module.exports = grunt =>
 			options:
 			{
 				url: process.env.FTP_URL,
-				command: 'mirror {SOURCE} {TARGET} --reverse --delete-first --parallel=10 --use-pget-n=10; chmod 0444 config.php; exit'
+				command: 'mirror {SOURCE} {TARGET} --reverse --delete-first --parallel=10 --use-pget-n=10; chmod 0444 www/htdocs/w00976cb/redaxscript-demo/config.php; exit'
 			}
 		}
 	};
