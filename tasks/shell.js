cf4056ac3a6ac781b11a79e45bdf8ed609c1e110
redaxmedia <info@redaxmedia.com>
2018-12-16 13:52:19 +0100

    Remove install for Dialog module

--- tasks/shell.js
+++ tasks/shell.js
@@ -39,7 +39,6 @@ module.exports = () =>
 			[
 				'cd build',
 				'php console.php install module --alias Analytics --halt-on-error',
-				'php console.php install module --alias Dialog --halt-on-error',
 				'php console.php install module --alias Demo --halt-on-error'
 			]
 			.join('&&')
